65623adb67163603706b141d444a8fd3c11e4c52
chandan641 <95743572+chandan641@users.noreply.github.com>
2021-12-13 11:14:12 +0530

    Update and rename codecoverageanalysis.ipnb to codecoverageanalysis.py

---
+++ codecoverageanalysis.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+data = {'EmpID':[111,112,113,114,115,116,117,118],
+        'EmpFirstName':['Ravi', 'Shubham', 'Rishabh', 'Sandeep', 'Sourabh', 'Sumanth', 'Kuldeep', 'Ajay'],
+        'EmpLastName':['Jha', 'Singh', 'Sharma', 'Dhama', 'Gupta', 'Kumar', 'Negi', 'Kanojiya'],
+        'MonthlySalary':[45000.00, 50000.00, 65000.00, 43000.00, 80000.00, 44000.00, 76000.00, 56000.00,]
+}
+
+def loadData():
+  dataframe = pd.DataFrame(data)
+  return dataframe
+
+def calculateAnnualPackage():
+  dataframe = loadData()
+  yearlyPkg =[]
+  for msalary in dataframe['MonthlySalary']:
+        yearlyPkg.append(msalary*12+50000)
+  dataframe['YearlyPackage'] = yearlyPkg
+  for indx in dataframe['YearlyPackage'].index:
+        print(f"{dataframe['EmpFirstName'][indx]} {dataframe['EmpLastName'][indx]} Yearly Package is {dataframe['YearlyPackage'][indx]}")
+  return dataframe
+
+calculateAnnualPackage()
