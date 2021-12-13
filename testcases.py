a275551681e6376d52633654df572c4154ebf245
chandan641 <95743572+chandan641@users.noreply.github.com>
2021-12-13 11:19:13 +0530

    Update testcases.py

--- testcases.py
+++ testcases.py
@@ -1,4 +1,9 @@
 import math
+from codecoverageanalysis import calculateAnnualPackage
+
+def test_pckg():
+   df = calculateAnnualPackage()
+   assert df['YearlyPackage'][0] == 590000
 
 def test_sqrt():
    num = 25
